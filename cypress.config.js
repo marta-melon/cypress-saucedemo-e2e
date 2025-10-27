18c0ce4cfd8359662037abc457bbf86426036972
Marta <marta.melon@gmail.com>
2025-10-27 14:22:15 +0100

    test: fix commands to use Sel

--- cypress.config.js
+++ cypress.config.js
@@ -7,10 +7,8 @@ module.exports = defineConfig({
     pageLoadTimeout: 60000,
     defaultCommandTimeout: 10000,
     specPattern: 'cypress/e2e/*.cy.js',
-    excludeSpecPattern: ['cypress/e2e/ref-*.cy.js'],
-    setupNodeEvents(on, config) {
-      // Keep Node-only plugins here. Do NOT import 'node:*' modules in browser code.
-      return config;
+        setupNodeEvents(on, config) {
+            return config;
     },
   },
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,33 +1,42 @@
+// cypress/support/commands.js
 import 'cypress-axe';
+import { Sel } from './selectors.js';
+
 
 Cypress.Commands.add('login', (username = 'standard_user', password = 'secret_sauce') => {
-  cy.visit('https://www.saucedemo.com/');
-  cy.get('[data-test="username"]').clear().type(username);
-  cy.get('[data-test="password"]').clear().type(password, { log: false });
-  cy.get('[data-test="login-button"]').click();
+  cy.visit('/'); // or: cy.visit('https://www.saucedemo.com/')
+  cy.get(Sel.login.username, { timeout: 15000 }).clear().type(username);
+  cy.get(Sel.login.password).clear().type(password, { log: false });
+  cy.get(Sel.login.submit).click();
   cy.url().should('include', '/inventory.html');
 });
 
+/** Logs out via burger menu and verifies redirect to the login page. */
 Cypress.Commands.add('logout', () => {
-  cy.get('#react-burger-menu-btn').click();
-  cy.get('#logout_sidebar_link').should('be.visible').click();
-  cy.url().should('match', /https:\/\/www\.saucedemo\.com\/?$/);
+  cy.get(Sel.burger.menuBtn).click();
+  cy.get(Sel.burger.logout).should('be.visible').click();
+  cy.url().should('eq', Cypress.config('baseUrl') + "/");
 });
 
+
 Cypress.Commands.add('ensureOnInventory', () => {
-  cy.login();
+  cy.location('pathname').then((p) => {
+    if (!p.includes('/inventory.html')) {
+      cy.login();
+    }
+  });
   cy.url().should('include', '/inventory.html');
   cy.contains('.title', 'Products').should('be.visible');
 });
 
+/** Adds the first visible product from the inventory list. */
 Cypress.Commands.add('addAnyItem', () => {
-  cy.get('.inventory_item').first().within(() => {
-    cy.contains('Add to cart').click();
+  cy.get(Sel.inventory.item).first().within(() => {
+    cy.contains('button', 'Add to cart').click();
   });
 });
 
-// NEW: openCart helper used by your existing specs
 Cypress.Commands.add('openCart', () => {
-  cy.get('.shopping_cart_link').click();
+  cy.get(Sel.inventory.cartIcon).click();
   cy.url().should('include', '/cart.html');
 });
