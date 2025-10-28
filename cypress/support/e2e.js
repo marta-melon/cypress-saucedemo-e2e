f0173bb3faa2227605cd546f148c194fc0c9aaa9
Marta <marta.melon@gmail.com>
2025-10-28 14:33:59 +0100

    test(a11y): remove duplicate inventory accessibility test from quality-a11y.cy.js

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,2 +1,10 @@
 import 'cypress-axe';
 import './commands';
+
+import { addMatchImageSnapshotCommand } from '@simonsmith/cypress-image-snapshot/command'
+
+addMatchImageSnapshotCommand()
+
+addMatchImageSnapshotCommand({
+  failureThreshold: 0.2, // threshold for test to fail if change exceeds 0.2%
+})
